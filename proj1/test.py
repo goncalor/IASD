992f488d944d0b1a881de3e0cf0610afa6f09505
okward <rikehmmc@gmail.com>
2015-10-11 19:29:24 +0100

    Testing updated
    test for the sorting functions

--- proj1/test.py
+++ proj1/test.py
@@ -2,43 +2,27 @@
 import graph
 import edge
 import node
+import edgeinfo
 
-mygraph= graph.Graph(3)
+mygraph= graph.Graph(3+1)
 
-mygraph.addEdge(1, 2, 1, 1, 10, 100, 200, 50)
-mygraph.addEdge(2, 3, 2, 3, 20, 100, 200, 50)
-mygraph.addEdge(3, 1, 3, 2, 30, 100, 200, 50)
+info= edgeinfo.EdgeInfo( 0, 3, 10, 100, 200, 50)
+mygraph.addEdge(1, 2, info)
 
+info= edgeinfo.EdgeInfo( 0, 5, 20, 100, 200, 50)
+mygraph.addEdge(2, 3, info)
 
-#print all nodes and their info
-for i in range(len(mygraph.nodes)):
-    print('NODE ' + str(i))
-    print('nodeID ' + str(mygraph.nodes[i].id))
-    
-    for j in range(len(mygraph.nodes[i].neigh)):
-        #if this is the nodeA print nodeB
-        if mygraph.nodes[i].neigh[j].nodeA.id == mygraph.nodes[i].id:
-            print('\tneighbour: ' + str(mygraph.nodes[i].neigh[j].nodeB.id))
-        #else this is nodeB, print nodeA
-        else:
-            print('\tneighbour: ' , mygraph.nodes[i].neigh[j].nodeA.id)    
-    
-#print all edges and their info
-for i in range(len(mygraph.edges)):
-    print('EDGE ' , i)
-    print('nodeA ' , mygraph.edges[i].nodeA.id )
-    print('nodeB ' , mygraph.edges[i].nodeB.id)
-    print('trnas ' , mygraph.edges[i].transType)
-    print('duration ', mygraph.edges[i].duration)
-    print('price ' , mygraph.edges[i].price)
-    print('ti ' , mygraph.edges[i].ti)
-    print('tf ' , mygraph.edges[i].tf)
-    print('period ' , mygraph.edges[i].period)
-    print('\n\n')
+info= edgeinfo.EdgeInfo( 0, 4, 30, 100, 200, 50)
+mygraph.addEdge(3, 1, info)
 
+print('ola\n')
 
-print(mygraph.sorted_price()[0].nodeA.id, mygraph.sorted_price()[0].nodeB.id)
-print(mygraph.sorted_price()[1].nodeA.id, mygraph.sorted_price()[1].nodeB.id)
-print(mygraph.sorted_price()[2].nodeA.id, mygraph.sorted_price()[2].nodeB.id)
+print(mygraph.sorted_price()[0].nodeA, mygraph.sorted_price()[0].nodeB)
+print(mygraph.sorted_price()[1].nodeA, mygraph.sorted_price()[1].nodeB)
+print(mygraph.sorted_price()[2].nodeA, mygraph.sorted_price()[2].nodeB)
 
-    
+print('\n')
+
+print(mygraph.sorted_duration()[0].nodeA, mygraph.sorted_duration()[0].nodeB)
+print(mygraph.sorted_duration()[1].nodeA, mygraph.sorted_duration()[1].nodeB)
+print(mygraph.sorted_duration()[2].nodeA, mygraph.sorted_duration()[2].nodeB)
