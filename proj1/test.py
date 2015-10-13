9153eab6c212319b4ee92e03ecb80b465fe847ed
okward <rikehmmc@gmail.com>
2015-10-13 12:24:28 +0100

    New tests

--- proj1/test.py
+++ proj1/test.py
@@ -3,17 +3,31 @@ import graph
 import edge
 import node
 import edgeinfo
+from copy import deepcopy
 
-mygraph= graph.Graph(3+1)
+mygraph= graph.Graph(6+1)
 
-info= edgeinfo.EdgeInfo( 0, 3, 10, 100, 200, 50)
+#http://www.csse.monash.edu.au/~lloyd/tildeAlgDS/Graph/PICS/WtUndMST5.gif
+info= edgeinfo.EdgeInfo( 0, 3, 4, 100, 200, 50)
+mygraph.addEdge(1, 4, info)
+info= edgeinfo.EdgeInfo( 0, 4, 5, 100, 200, 50)
+mygraph.addEdge(2, 4, info)
+info= edgeinfo.EdgeInfo( 0, 4, 5, 100, 200, 50)
+mygraph.addEdge(2, 5, info)
+
+info= edgeinfo.EdgeInfo( 0, 4, 5, 100, 200, 50)
 mygraph.addEdge(1, 2, info)
-
-info= edgeinfo.EdgeInfo( 0, 5, 20, 100, 200, 50)
+info= edgeinfo.EdgeInfo( 0, 3, 4, 100, 200, 50)
 mygraph.addEdge(2, 3, info)
+info= edgeinfo.EdgeInfo( 0, 2, 3, 100, 200, 50)
+mygraph.addEdge(3, 4, info)
+info= edgeinfo.EdgeInfo( 0, 3, 4, 100, 200, 50)
+mygraph.addEdge(4, 5, info)
+info= edgeinfo.EdgeInfo( 0, 1, 2, 100, 200, 50)
+mygraph.addEdge(5, 6, info)
+info= edgeinfo.EdgeInfo( 0, 5, 6, 100, 200, 50)
+mygraph.addEdge(6, 1, info)
 
-info= edgeinfo.EdgeInfo( 0, 4, 30, 100, 200, 50)
-mygraph.addEdge(3, 1, info)
 
 print('ola\n')
 
@@ -26,3 +40,7 @@ print('\n')
 print(mygraph.sorted_duration()[0].nodeA, mygraph.sorted_duration()[0].nodeB)
 print(mygraph.sorted_duration()[1].nodeA, mygraph.sorted_duration()[1].nodeB)
 print(mygraph.sorted_duration()[2].nodeA, mygraph.sorted_duration()[2].nodeB)
+
+
+print(mygraph.edges[0].nodeA.id_, mygraph.edges[0].nodeB.id_)
+print(mygraph.edges[0].neighbour(mygraph.nodes[1]))
