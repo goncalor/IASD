dbcbc76086ef0d080ec1cc04376c85dea929f6f4
okward <rikehmmc@gmail.com>
2015-10-13 12:22:13 +0100

    Refined equals function for Edge
    equals now considers nodeA, nodeB, price and duration

--- proj1/edge.py
+++ proj1/edge.py
@@ -14,12 +14,23 @@ class Edge:
 		self.nodeB = nodeB
 		self.info = info
 
-"""
+	def neighbour(self, node):
+		if node == self.nodeA:
+			return self.nodeB
+		elif node == self.nodeB:
+			return self.nodeA
+		else:
+			return None
+
+
 	def __eq__(self, edge):
+		#check if the nodes the edge connects are the same
 		if (self.nodeA == edge.nodeA and self.nodeB == edge.nodeB) \
-				or (self.nodeA == edge.nodeB && self.nodeB == edge.nodeA):
-			return True
+				or (self.nodeA == edge.nodeB and self.nodeB == edge.nodeA):
+			#if the the edges connected and the price and duration are the same, the edge is the same
+			if self.info.price == edge.info.price and self.info.duration == edge.info.duration:
+				return True
 		else:
 			return False
-"""			
+
 
