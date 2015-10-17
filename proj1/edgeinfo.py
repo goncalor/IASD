7e986ec044115d6e820c28e2cf7ca64e823ba83b
okward <rikehmmc@gmail.com>
2015-10-17 18:49:10 +0100

    EdgeInfo adapted to Transport class

--- proj1/edgeinfo.py
+++ proj1/edgeinfo.py
@@ -2,19 +2,14 @@
 """ Defines the information on an Edge of a Graph. """
 
 import sys
+from transport import Transport
 
 class EdgeInfo:
 
-	transType= None
-	duration= sys.maxsize
-	price= sys.maxsize
-	ti= sys.maxsize
-	tf= sys.maxsize
-	period= sys.maxsize
 
 	def __init__(self, transType, duration, price, ti, tf, period):
 
-		self.transType = transType
+		self.transType = Transport(transType)
 		self.duration = duration
 		self.price = price
 		self.ti = ti
