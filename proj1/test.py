7e9a1d3a9540a7d0a5f11b85b5d9c01f027ad5c6
okward <rikehmmc@gmail.com>
2015-10-17 18:53:59 +0100

    Test for removeEdges
    One more test file

--- proj1/test.py
+++ proj1/test.py
@@ -11,27 +11,27 @@ mygraph= graph.Graph(6+1)
 
 #http://www.csse.monash.edu.au/~lloyd/tildeAlgDS/Graph/PICS/WtUndMST5.gif
 #duration price
-info= edgeinfo.EdgeInfo( 0, 3, 4, 100, 200, 50)
+info= edgeinfo.EdgeInfo( 'comboio', 3, 4, 100, 200, 50)
 mygraph.addEdge(1, 4, info)
-info= edgeinfo.EdgeInfo( 0, 4, 5, 100, 200, 50)
+info= edgeinfo.EdgeInfo( 'aviao', 4, 5, 100, 200, 50)
 mygraph.addEdge(2, 4, info)
-info= edgeinfo.EdgeInfo( 0, 4, 5, 100, 200, 50)
+info= edgeinfo.EdgeInfo( 'aviao', 4, 5, 100, 200, 50)
 mygraph.addEdge(2, 5, info)
 
 
-info= edgeinfo.EdgeInfo( 0, 4, 5, 100, 200, 50)
+info= edgeinfo.EdgeInfo( 'aviao', 4, 5, 100, 200, 50)
 mygraph.addEdge(1, 2, info)
-info= edgeinfo.EdgeInfo( 0, 20, 21, 100, 200, 50)
+info= edgeinfo.EdgeInfo( 'aviao', 20, 21, 100, 200, 50)
 mygraph.addEdge(1, 2, info)
-info= edgeinfo.EdgeInfo( 0, 3, 4, 100, 200, 50)
+info= edgeinfo.EdgeInfo( 'aviao', 3, 4, 100, 200, 50)
 mygraph.addEdge(2, 3, info)
-info= edgeinfo.EdgeInfo( 0, 2, 3, 100, 200, 50)
+info= edgeinfo.EdgeInfo( 'aviao', 2, 3, 100, 200, 50)
 mygraph.addEdge(3, 4, info)
-info= edgeinfo.EdgeInfo( 0, 3, 4, 100, 200, 50)
+info= edgeinfo.EdgeInfo( 'aviao', 3, 4, 100, 200, 50)
 mygraph.addEdge(4, 5, info)
-info= edgeinfo.EdgeInfo( 0, 1, 2, 100, 200, 50)
+info= edgeinfo.EdgeInfo( 'aviao', 1, 2, 100, 200, 50)
 mygraph.addEdge(5, 6, info)
-info= edgeinfo.EdgeInfo( 0, 5, 6, 100, 200, 50)
+info= edgeinfo.EdgeInfo( 'aviao', 5, 6, 100, 200, 50)
 mygraph.addEdge(6, 1, info)
 
 
@@ -39,13 +39,16 @@ print('ola\n')
 
 print(mygraph)
 
-myheuristic= heuristic.Heuristic(mygraph, 'duration')
 
 
-start=3
-goal=6
-print(myheuristic.heurIST_it(mygraph.nodes[start], mygraph.nodes[goal]))
+grafo_sem_avioes= mygraph.removeEdges('transType', 'aviao')
 
+print(grafo_sem_avioes)
+
+print('edges sem avioes', len(grafo_sem_avioes.edges))
+
+for node in grafo_sem_avioes.nodes:
+    print('node', node.id_, node.neigh)
 
 
 
