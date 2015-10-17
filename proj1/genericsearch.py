34a17c73242fd428a6820ed74b8e665a06a2ca80
Gonçalo Ribeiro <darklaw13@yahoo.co.uk>
2015-10-17 10:55:53 +0100

    generic_search() now accepts methods to expand,etc

--- proj1/genericsearch.py
+++ proj1/genericsearch.py
@@ -1,6 +1,6 @@
 import sys
 
-def generic_search(graph, start_node, goal):
+def generic_search(graph, start_node, goal, remove, expand, isgoal, path):
 
 	fringe = [(0, start_node)]
 	# save the node id and the transportation type
@@ -20,50 +20,4 @@ def generic_search(graph, start_node, goal):
 
 	# no path found
 	return []
-
-
-def path(start, goal, parents):
-	path = []
-	node = goal.id_
-
-	while node != start.id_:
-		path.append(parents[node])
-		node = parents[node][0]
-	return path + [(start.id_, None)]
-
-
-def expand(curr, fringe, parents, known_costs):
-	""" Expands the nodes that are neighbours of 'curr.' """
-	for edge in curr.neigh:
-		neigh = edge.neighbour(curr)
-
-		# the cost of the neighbour is this curr's cost plus the edge cost
-		neigh_known_cost = known_costs[curr.id_] + edge.info.price
-		#heur = heuristic(relaxed_graph, neigh, goal)
-		heur = 0
-
-		# if a new best path was found to neigh
-		if known_costs[neigh.id_] > neigh_known_cost + heur:
-			# update the cost to reach neigh
-			known_costs[neigh.id_] = neigh_known_cost
-			# add neigh to the fringe
-			fringe.append((neigh_known_cost + heur, neigh))
-			# update neigh's parent
-			parents[neigh.id_] = (curr.id_, edge.info.transType)
-
-	# "Sorts are guaranteed to be stable."
-	fringe.sort(key=lambda tup: tup[0], reverse=True)
-
-	print([(item[1].id_, item[0]) for item in fringe])
-	
-
-def isgoal(node, goal):
-	if node == goal:
-		return True
-	return False
-
-
-def remove(fringe):
-	""" Returns only the node from the (f, node) tuple """
-	return fringe.pop()[1]
 
--- proj1/main.py
+++ proj1/main.py
@@ -1,6 +1,7 @@
 # our created modules
 import iofiles
 import genericsearch
+from searchcriteria import SearchCriteria
 
 # other modules
 import argparse
@@ -24,9 +25,13 @@ if __name__ == "__main__":
 
 	#pprint(clients_list)
 
+
 	client_no = 0
+	sc = SearchCriteria(clients_list[client_no])
+
 	ans = genericsearch.generic_search(graph,
 			graph.nodes[clients_list[client_no]['from']],
-			graph.nodes[clients_list[client_no]['to']])
+			graph.nodes[clients_list[client_no]['to']],
+			sc.remove, sc.expand, sc.isgoal, sc.path)
 
 	print(ans)
