8361be466f0bd5ba1483adb8efe3609882bdf99d
okward <rikehmmc@gmail.com>
2015-11-18 21:57:17 +0000

    Timed tests

--- proj2/test.py
+++ proj2/test.py
@@ -5,17 +5,23 @@ from cnf_kb import CnfKb
 from wsat import WSat
 import iofiles
 from gsat import GSat
+import time
 
 
-kb3 = iofiles.read_kb('problems/uf20-01.cnf')
+kb3 = iofiles.read_kb('problems/uf50-01.cnf')
 
 print(kb3)
 
 walk = WSat(kb3, 0.5, 1000)
 greedy = GSat(kb3, 5, 200)
 
+start = time.time()
 print(walk.solve())
+print(str(time.time() - start) + ' seconds')
+
+start = time.time()
 print(greedy.solve())
+print(str(time.time() - start) + ' seconds')
 
 
 
