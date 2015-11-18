218d8042f77ad919a9e1ced3a9f74d4397183112
Gonçalo Ribeiro <darklaw13@yahoo.co.uk>
2015-11-18 23:36:21 +0000

    Reindented main. Solves sentences in batches.

--- proj2/main.py
+++ proj2/main.py
@@ -12,44 +12,46 @@ parser.add_argument('--gsat', '-gs', action='store_true', help='use GSAT')
 parser.add_argument('--wsat', '-ws', action='store_true', help='use WalkSAT')
 parser.add_argument('--dpll', '-dp', action='store_true', help='use DPLL algorithm')
 parser.add_argument('sentence', type=argparse.FileType('r'), nargs='+', \
-		help='a file in DIMASC format, containing a sentence')
+        help='a file in DIMASC format, containing a sentence')
 args = parser.parse_args()
 
 if __name__ == "__main__":
 
-	print('Reading sentence... ', end = '')
-	# TODO
-	print('Done.\n')
 
-	# measure time
-	start_time_program = time.time()
+    # measure time
+    start_time_program = time.time()
 
-	if args.gsat:
-		print('Solving with GSAT... ', end = '')
-		start_time = time.time()
+    for f in args.sentence:
+        print('Reading sentence from ' + f.name + '... ', end = '')
+        # TODO
+        print('Done.\n')
 
-		print('Done.')
-		print("GSAT time:", time.time() - start_time, '\n')
+        if args.gsat:
+            print('Solving with GSAT... ', end = '')
+            start_time = time.time()
 
-	if args.wsat:
-		print('Solving with WalkSAT... ', end = '')
-		start_time = time.time()
+            print('Done.')
+            print("GSAT time:", time.time() - start_time, '\n')
 
-		print('Done.')
-		print("WalkSAT time:", time.time() - start_time, '\n')
+        if args.wsat:
+            print('Solving with WalkSAT... ', end = '')
+            start_time = time.time()
 
-	if args.dpll:
-		print('Solving with DPLL... ', end = '')
-		start_time = time.time()
+            print('Done.')
+            print("WalkSAT time:", time.time() - start_time, '\n')
 
-		print('Done.')
-		print("DPLL time:", time.time() - start_time, '\n')
+        if args.dpll:
+            print('Solving with DPLL... ', end = '')
+            start_time = time.time()
+
+            print('Done.')
+            print("DPLL time:", time.time() - start_time, '\n')
 
 
-	# write solutions to a file
-	print('Preserving solutions... ', end = '')
-	# TODO: save solution
-	print('Done.')
+    # write solutions to a file
+    print('Preserving solutions... ', end = '')
+    # TODO: save solution
+    print('Done.')
 
-	print("\nTotal time:", time.time() - start_time_program)
+    print("\nTotal time:", time.time() - start_time_program)
 
